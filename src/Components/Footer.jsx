2bc4d29d922f7264a1ebe6a1499cfec2633b7ff3
BrightNeon7631 <94930422+BrightNeon7631@users.noreply.github.com>
2023-06-28 17:01:06 +0200

    fix footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <footer>©2023 #VANLIFE</footer>
+    )
+}
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from '../Components/Header';
+import Footer from "./Footer";
 
 export default function Layout() {
     return (
-      <>
-        <Header />
-        <Outlet />
-      </>
+      <div className="site--layout">
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
     );
 }
